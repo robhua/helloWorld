99a6ef0cf77b77f0c5c0180d9d9db67cc8bc5916
robhua <hunghm.ce@gmail.com>
2018-06-27 14:06:13 +0700

    update name branch

--- src/HelloWorld.c
+++ src/HelloWorld.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Develop branch!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
